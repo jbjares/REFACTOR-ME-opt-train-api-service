109a650538f5a50a364c97a8ce23db8900949172
lukaszimmermann <lukas.zimmermann@informatik.uni-tuebingen.de>
2018-09-07 12:08:42 +0200

    Init: Project

---
+++ src/main/kotlin/de/difuture/ekut/pht/service/trainapi/TrainapiApplication.kt
@@ -0,0 +1,11 @@
+package de.difuture.ekut.pht.service.trainapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrainapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TrainapiApplication>(*args)
+}
---
+++ src/test/kotlin/de/difuture/ekut/pht/service/trainapi/TrainapiApplicationTests.kt
@@ -0,0 +1,16 @@
+package de.difuture.ekut.pht.service.trainapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class TrainapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
